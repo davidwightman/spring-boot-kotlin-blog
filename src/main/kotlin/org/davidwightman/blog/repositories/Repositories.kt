6361617bc4b76216df6ad9d8e9a107e1bb8a2968
David Wightman <david@davidwightman.org>
2020-08-11 08:03:31 -0400

    add users and articles

---
+++ src/main/kotlin/org/davidwightman/blog/repositories/Repositories.kt
@@ -0,0 +1,14 @@
+package org.davidwightman.blog.repositories
+
+import org.davidwightman.blog.model.Article
+import org.davidwightman.blog.model.User
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+    fun findBySlug(slug: String): Article?
+    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, Long> {
+    fun findByLogin(login: String): User?
+}
